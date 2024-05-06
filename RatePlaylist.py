67c45541428a21f8ce7553a9c47ec820162c1cd4
FunkyKwak <38962398+FunkyKwak@users.noreply.github.com>
2024-05-06 22:04:49 +0200

    Update RatePlaylist.py

--- RatePlaylist.py
+++ RatePlaylist.py
@@ -2,3 +2,9 @@ import connexion
 from plexapi.server import PlexServer
 
 plex = PlexServer(connexion.baseurl, connexion.token)
+
+playlist = plex.playlist("2024.03 - Chine")
+
+foreach(photo in playlist.items()):
+  print(photo.title)
+  #photo.rate(6.0)
