16e272f16d4789a1b0a66507a4578a62a4b3ff89
FunkyKwak <38962398+FunkyKwak@users.noreply.github.com>
2024-05-06 22:14:11 +0200

    Declare parameters out of the code

--- RatePlaylist.py
+++ RatePlaylist.py
@@ -1,10 +1,22 @@
 import connexion
 from plexapi.server import PlexServer
 
+
+########################################
+## Paramètres à modifier 
+########################################
+
+playlistName = "2024.03 - Chine"
+rating = 6.0 # sur 10, en float
+
+########################################
+
+
+
+
 plex = PlexServer(connexion.baseurl, connexion.token)
 
-playlist = plex.playlist("2024.03 - Chine")
-
+playlist = plex.playlist(playlistName)
 foreach(photo in playlist.items()):
   print(photo.title)
-  #photo.rate(6.0)
+  #photo.rate(rating)
