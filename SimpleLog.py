4667c3b89a3aabb8910c43b11ec429a514b14acf
Yann <crazy_rocker78@hotmail.fr>
2025-08-24 23:22:35 +0200

    TELEGRAM_TOKEN error

--- SimpleLog.py
+++ SimpleLog.py
@@ -14,10 +14,12 @@ logging.basicConfig(
 
 
 
+token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["TELEGRAM_CHAT_ID"]
+
+
 
 def send_telegram_message(message: str):
-    token = os.environ["TELEGRAM_TOKEN"]
-    chat_id = os.environ["TELEGRAM_CHAT_ID"]
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     requests.post(url, data={"chat_id": chat_id, "text": message})
 
